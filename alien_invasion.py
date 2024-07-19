9167b67fd7f34d6e43b97a54774efd58cf27c8bb
aleksinho <aleks.gsyan@gmail.com>
2024-07-19 11:40:27 +0400

    starting the main loop

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,3 +12,14 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """ Start the main loop for the game """
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Make the most recently drawn screen visible
+            pygame.display.flip()
