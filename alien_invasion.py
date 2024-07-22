7a1c034a3a6b8aa42671f04af2afbdf7322531a4
aleksinho <157628518+aleksinho@users.noreply.github.com>
2024-07-22 21:36:59 +0400

    bug fix

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,7 +14,7 @@ class AlienInvasion:
         self.settings = Settings()
         self.clock = pygame.time.Clock()
 
-        # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         # # Fullscreen mode
