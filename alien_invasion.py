859a0b82e6010951bbfee2ed890372ea56ce6e26
aleksinho <aleks.gsyan@gmail.com>
2024-07-22 22:37:01 +0400

    nothing

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from settings import Settings
 from ship import Ship
 
 
+
 class AlienInvasion:
     """ Overal class to manage game assets and behavior """
 
@@ -23,7 +24,6 @@ class AlienInvasion:
         # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         # self.settings.screen_width = self.screen.get_rect().width
         # self.settings.screen_height = self.screen.get_rect().height
-
 
         self.ship = Ship(self)
 
