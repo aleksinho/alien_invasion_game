5834f40b3858660def92b5543288b288b268a7db
aleksinho <aleks.gsyan@gmail.com>
2024-07-27 15:57:17 +0400

    creating _update_bullets() meth

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,13 +33,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # Get rid of bullets that disappear
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-
+            self._update_bullets()
             self._update_screen()
             # fps
             self.clock.tick(60)
@@ -81,6 +75,16 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """ Update position of bullets and get rid of old bullets. """
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullet that disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """ Update images on the screen, and flip to the new screen. """
