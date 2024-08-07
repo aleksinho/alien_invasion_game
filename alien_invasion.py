3ef9adc3057434fd08ad87f737402ae01944c8c6
aleksinho <aleks.gsyan@gmail.com>
2024-08-07 16:14:29 +0400

    dropping the fleet and changing the direction

--- alien_invasion.py
+++ alien_invasion.py
@@ -90,9 +90,6 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-    
-    def _update_aliens(self):
-        self.aliens.update()
 
     def _create_fleet(self):
         """ Create the fleet of the aliens """
@@ -118,6 +115,24 @@ class AlienInvasion:
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
+
+    def _check_fleet_endges(self):
+        """ Respond appropriatly if any aliens have reached an edge. """
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        """ Drop the entire fleet and change the fleet's direction. """
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+    def _update_aliens(self):
+        """ Check if the fleet is at an edge, then update the position. """
+        self._check_fleet_endges()
+        self.aliens.update()
 
     def _update_screen(self):
         """ Update images on the screen, and flip to the new screen. """
