d2175a2cf5e8ee68fd6be091a5c68704fdff619f
aleksinho <aleks.gsyan@gmail.com>
2024-08-07 15:48:58 +0400

    checking whether an alien has hit the edge

--- alien.py
+++ alien.py
@@ -22,7 +22,12 @@ class Alien(Sprite):
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
 
+    def check_edges(self):
+        """ Return True if alien is at the edge of the screen. """
+        screen_rect = self.screen.get_rect()
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+
     def update(self):
-        """ Move the alien to the right """
-        self.x += self.settings.alien_speed
+        """ Move the alien right or left"""
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
         self.rect.x = self.x
--- settings.py
+++ settings.py
@@ -20,3 +20,6 @@ class Settings:
 
         # Alien settings
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
